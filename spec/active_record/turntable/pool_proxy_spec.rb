0cc4d740440813f2055ca872e613e2127d3b4b83
MATSUBARA Nobutada <nobutada0507@gmail.com>
2024-05-17 13:56:00 +0900

    update unsupported proxy method in test

--- spec/active_record/turntable/pool_proxy_spec.rb
+++ spec/active_record/turntable/pool_proxy_spec.rb
@@ -4,7 +4,8 @@ describe ActiveRecord::Turntable::PoolProxy do
   context "When initialized" do
     subject { ActiveRecord::Turntable::PoolProxy.new(nil) }
 
-    UNSUPPORTED_PROXY_METHODS = %i[checkout checkin stat lock_thread= remove num_waiting_in_queue].freeze
+    # TODO: should support AR 6.1 methods (connection_klass discarded? pool_config db_config)
+    UNSUPPORTED_PROXY_METHODS = %i[checkout checkin stat lock_thread= remove num_waiting_in_queue connection_klass discarded? pool_config db_config].freeze
 
     context "Comparing original connection pool" do
       (ActiveRecord::ConnectionAdapters::ConnectionPool.instance_methods(false) - UNSUPPORTED_PROXY_METHODS).each do |original_method|
