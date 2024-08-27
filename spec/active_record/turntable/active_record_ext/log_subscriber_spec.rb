6fa2ffa04b1d48859ff523ca98c3a9cc36f4f9e1
MATSUBARA Nobutada <nobutada0507@gmail.com>
2024-08-27 13:17:02 +0900

    Fix: Struct kwargs for Ruby 3.2

--- spec/active_record/turntable/active_record_ext/log_subscriber_spec.rb
+++ spec/active_record/turntable/active_record_ext/log_subscriber_spec.rb
@@ -34,23 +34,23 @@ describe ActiveRecord::Turntable::ActiveRecordExt::LogSubscriber do
     it "ignore SCHEMA log" do
       expect(subscriber.debugs.length).to eq 0
 
-      subscriber.sql(TestEvent.new(name: "bar", turntable_shard_name: "shard_1"))
+      subscriber.sql(TestEvent.new({name: "bar", turntable_shard_name: "shard_1"}))
       expect(subscriber.debugs.length).to eq 1
 
-      subscriber.sql(TestEvent.new(name: "SCHEMA", turntable_shard_name: "shard_1"))
+      subscriber.sql(TestEvent.new({name: "SCHEMA", turntable_shard_name: "shard_1"}))
       expect(subscriber.debugs.length).to eq 1
     end
 
     context "When payload name is `SQL`" do
       it "logs in MAGENTA color" do
-        subscriber.sql(TestEvent.new(name: "SQL", turntable_shard_name: "shard_1"))
+        subscriber.sql(TestEvent.new({name: "SQL", turntable_shard_name: "shard_1"}))
         expect(subscriber.debugs.first).to match(/#{REGEXP_MAGENTA}SQL \(0\.7ms\) \[Shard: shard_1\]#{REGEXP_CLEAR}/)
       end
     end
 
     context "When payload name is `Model Load`" do
       it "logs in CYAN color" do
-        subscriber.sql(TestEvent.new(name: "Model Load", turntable_shard_name: "shard_1"))
+        subscriber.sql(TestEvent.new({name: "Model Load", turntable_shard_name: "shard_1"}))
         expect(subscriber.debugs.first).to match(/#{REGEXP_CYAN}Model Load \(0\.7ms\) \[Shard: shard_1\]#{REGEXP_CLEAR}/)
       end
     end
@@ -59,7 +59,7 @@ describe ActiveRecord::Turntable::ActiveRecordExt::LogSubscriber do
       it "logs binds parameters" do
         binds = [ActiveRecord::Relation::QueryAttribute.new("id", 10, ActiveRecord::Type::Value.new)]
         type_casted_binds = [10]
-        subscriber.sql(TestEvent.new(name: "Model Load", binds: binds, type_casted_binds: type_casted_binds))
+        subscriber.sql(TestEvent.new({name: "Model Load", binds: binds, type_casted_binds: type_casted_binds}))
         expect(subscriber.debugs.first).to match(/\[\["id", 10\]\]/)
       end
     end
