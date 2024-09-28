978f5eb0c5fffb21626dd04776153fd3e0bd885b
MATSUBARA Nobutada <nobutada0507@gmail.com>
2024-09-28 20:38:21 +0900

    update override method for AR7.0

--- lib/active_record/turntable/active_record_ext/log_subscriber.rb
+++ lib/active_record/turntable/active_record_ext/log_subscriber.rb
@@ -12,29 +12,39 @@ module ActiveRecord::Turntable
 
         return if ActiveRecord::LogSubscriber::IGNORE_PAYLOAD_NAMES.include?(payload[:name])
 
-        name  = "#{payload[:name]} (#{event.duration.round(1)}ms)"
+        name = if payload[:async]
+          "ASYNC #{payload[:name]} (#{payload[:lock_wait].round(1)}ms) (db time #{event.duration.round(1)}ms)"
+        else
+          "#{payload[:name]} (#{event.duration.round(1)}ms)"
+        end
         name  = "#{name} [Shard: #{payload[:turntable_shard_name]}]" if payload[:turntable_shard_name]
         name  = "CACHE #{name}" if payload[:cached]
         sql   = payload[:sql]
         binds = nil
 
-        unless (payload[:binds] || []).empty?
-          if Util.ar_version_equals_or_later?("5.0.3")
-            casted_params = if Util.ar_version_satisfy?(">= 5.1.5") || Util.ar_version_satisfy?([">= 5.0.7", "< 5.1"])
-                              type_casted_binds(payload[:type_casted_binds])
-                            else
-                              type_casted_binds(payload[:binds], payload[:type_casted_binds])
-                            end
-            binds = "  " + payload[:binds].zip(casted_params).map { |attr, value|
-              render_bind(attr, value)
-            }.inspect
-          else
-            binds = "  " + payload[:binds].map { |attr| render_bind(attr) }.inspect
+        if payload[:binds]&.any?
+          casted_params = type_casted_binds(payload[:type_casted_binds])
+
+          binds = []
+          payload[:binds].each_with_index do |attr, i|
+            attribute_name = if attr.respond_to?(:name)
+                               attr.name
+                             elsif attr.respond_to?(:[]) && attr[i].respond_to?(:name)
+                               attr[i].name
+                             else
+                               nil
+                             end
+
+            filtered_params = filter(attribute_name, casted_params[i])
+
+            binds << render_bind(attr, filtered_params)
           end
+          binds = binds.inspect
+          binds.prepend("  ")
         end
 
         name = colorize_payload_name(name, payload[:name])
-        sql  = color(sql, sql_color(sql), true) if Util.ar60_or_later? && colorize_logging
+        sql  = color(sql, sql_color(sql), true) if colorize_logging
 
         debug "  #{name}  #{sql}#{binds}"
       end
